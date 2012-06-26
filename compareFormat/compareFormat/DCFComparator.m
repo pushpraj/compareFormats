f3992fdbbf1d717ef1034b90267395adfe2a0870
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-06-26 07:51:19 +0530

    Enable time logging.

--- compareFormat/compareFormat/DCFComparator.m
+++ compareFormat/compareFormat/DCFComparator.m
@@ -17,14 +17,24 @@
   
   NSArray *dictionaryObjects = [DCFDictionaryList dictionaryList];
   NSArray *comparatorList = [DCFComparingFormat allFormats];
+  NSDate *startEncode, *endEncode, *startParse, *endParse;
   for (DCFTestObject *testObject in dictionaryObjects) {
     NSDictionary *dictObject = testObject.object;
     NSLog(@"Object Info: %@", testObject.info);
     for (DCFComparingFormat *comparator in comparatorList) {
       NSLog(@"\tcomparator: %@", [comparator info]);
+      
+      startEncode = [NSDate date];
       NSData *data = [comparator encode:dictObject];
+      endEncode = [NSDate date];
+      NSLog(@"\t\tencoding time: %f", [endEncode timeIntervalSinceDate:startEncode]);
+      
       NSLog(@"\t\tdata length: %d", [data length]);
+      
+      startParse = [NSDate date];
       NSDictionary *dictRec = (NSDictionary *)[comparator parseData:data];
+      endParse = [NSDate date];
+      NSLog(@"\t\tParsing time: %f", [endParse timeIntervalSinceDate:startParse]);
       
       NSAssert([dictObject isEqualToDictionary:dictRec], @"generated objects are not equal!");
     }    
