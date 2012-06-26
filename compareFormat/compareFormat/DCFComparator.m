78c22338e006f2755d164c1d5317b039b2201f35
PushpRaj Agrawal <pushpraj.agrawal@gmail.com>
2012-06-26 08:07:29 +0530

    Pretty print logs.

--- compareFormat/compareFormat/DCFComparator.m
+++ compareFormat/compareFormat/DCFComparator.m
@@ -18,24 +18,23 @@
   NSArray *dictionaryObjects = [DCFDictionaryList dictionaryList];
   NSArray *comparatorList = [DCFComparingFormat allFormats];
   NSDate *startEncode, *endEncode, *startParse, *endParse;
+  NSLog(@"\t%18s: %20s %10s %20s", "Comparator", "Encoding time", "data size", "Decoding time" );
   for (DCFTestObject *testObject in dictionaryObjects) {
     NSDictionary *dictObject = testObject.object;
     NSLog(@"Object Info: %@", testObject.info);
     for (DCFComparingFormat *comparator in comparatorList) {
-      NSLog(@"\tcomparator: %@", [comparator info]);
       
       startEncode = [NSDate date];
       NSData *data = [comparator encode:dictObject];
       endEncode = [NSDate date];
-      NSLog(@"\t\tencoding time: %f", [endEncode timeIntervalSinceDate:startEncode]);
       
-      NSLog(@"\t\tdata length: %d", [data length]);
       
       startParse = [NSDate date];
       NSDictionary *dictRec = (NSDictionary *)[comparator parseData:data];
       endParse = [NSDate date];
-      NSLog(@"\t\tParsing time: %f", [endParse timeIntervalSinceDate:startParse]);
       
+      NSLog(@"\t%15s: %20f %10d %20f", [[comparator info] cStringUsingEncoding:NSASCIIStringEncoding], [endEncode timeIntervalSinceDate:startEncode], [data length], [endParse timeIntervalSinceDate:startParse]);
+
       NSAssert([dictObject isEqualToDictionary:dictRec], @"generated objects are not equal!");
     }    
   }
